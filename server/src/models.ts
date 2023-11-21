d0c57766db06af8231ae6cd58e0d274eabf3358a
Joseph Ogbole <on.josephinfo@gmail.com>
2023-11-21 10:55:02 +0100

    Apollo graphql lift-off 1-4 done

---
+++ server/src/models.ts
@@ -0,0 +1,23 @@
+export type TrackModel = {
+    id: string;
+    title: string;
+    authorId: string;
+    thumbnail: string;
+    length: number;
+    modulesCount: number;
+    description: string;
+    numberOfViews: number;
+  };
+  
+  export type AuthorModel = {
+    id: string;
+    name: string;
+    photo: string;
+  };
+  
+  export type ModuleModel = {
+    id: string;
+    title: string;
+    length: number;
+  }
+  
